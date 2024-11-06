47295c020cb59a85e5ce6049a3f4d345d6d31fb0
Joshua Brigati <brigati.joshua@gmail.com>
2024-11-06 17:04:31 -0600

    updated better for scrapper

---
+++ src/app/challenge-1/page.tsx
@@ -0,0 +1,12 @@
+import Challenge1 from "../components/challenge-1";
+
+
+const Challenge1Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Challenge1 />
+    </div>
+  )
+};
+
+export default Challenge1Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,8 @@ export default function Home() {
     <div className="flex flex-col gap-6 h-screen p-24">
       <h1 className="text-4xl font-bold">GreatFrontEnd Challenges</h1>
       <div className="flex flex-col gap-4">
-        <h2 className="text-2xl font-bold">Challenges</h2>
-        <div className="flex flex-col gap-4">
-          <h3 className="text-xl font-bold">Challenge 1</h3>
-          <Challenge1 />
-        </div>
+        <h3 className="text-lg font-semibold">Challenge 1</h3>
+        <Challenge1 />
       </div>
     </div>
   );
