ce8cede63fcbba62275a93b625d5149eb3965f77
Joshua Brigati <brigati.joshua@gmail.com>
2024-11-06 17:11:33 -0600

    more updates to make it lineup with their comparison

--- src/app/challenge-1/page.tsx
+++ src/app/challenge-1/page.tsx
@@ -3,7 +3,7 @@ import Challenge1 from "../components/challenge-1";
 
 const Challenge1Page = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col justify-center min-h-screen py-[200px]">
       <Challenge1 />
     </div>
   )
