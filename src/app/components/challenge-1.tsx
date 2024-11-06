9b8903de10683d4918d1b93f16a97c9594e8ae62
Joshua Brigati <brigati.joshua@gmail.com>
2024-11-06 16:27:15 -0600

    init with Challenge 1

---
+++ src/app/components/challenge-1.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+const Challenge1 = () => {
+  return (
+    <div className="flex flex-col gap-4 bg-white rounded-lg p-6 w-[340px]">
+      <div className="flex items-center gap-4">
+        <Image
+          src="/profile-thumbnail.png"
+          alt="avatar"
+          width={48}
+          height={48}
+        />
+        <div className="flex flex-col gap-px">
+          <h4 className="text-lg font-semibold text-neutral-900">Sarah Dole</h4>
+          <h6 className="text-sm text-neutral-600">@sarahdole</h6>
+        </div>
+      </div>
+      <p className="text-base text-neutral-600">
+        I've been searching for high-quality abstract images for my design
+        projects, and I'm thrilled to have found this platform. The variety
+        and depth of creativity are astounding!
+      </p>
+    </div>
+  );
+};
+
+export default Challenge1;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+  variable: "--font-noto-sans",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${notoSans.variable} antialiased`}>
         {children}
       </body>
     </html>
